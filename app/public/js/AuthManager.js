714900b7822a6b18db9a863ebd78633bed6d96b4
neo-darbellay <178883466+neo-darbellay@users.noreply.github.com>
2026-03-18 12:11:08 +0100

    feat(Étape 2): Implémenter une page d'inscription en frontend
    [10][Done]
    J'ai aussi passé les fonctions dans un fichier js commun à register et à login

---
+++ app/public/js/AuthManager.js
@@ -0,0 +1,65 @@
+const handleLogin = (event) => {
+  // Empêche le formulaire de se soumettre normalement
+  event.preventDefault();
+
+  // Récupère les données du formulaire
+  const formData = new FormData(event.target);
+
+  // Séparer les champs email et password
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  // Faire un POST sur /api/auth/login avec les données du formulaire
+  fetch("/api/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        window.location.href = "/";
+      } else {
+        alert("Email ou mot de passe incorrect.");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      alert("Erreur serveur");
+    });
+};
+
+const handleRegister = (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const username = formData.get("username");
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  fetch("/api/auth/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, email, password }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        alert("Compte créé avec succès !");
+        window.location.href = "/login.html";
+      } else {
+        alert("Erreur lors de l'inscription. Vérifiez vos informations.");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      alert("Erreur serveur");
+    });
+};
+
+document.getElementById("login-form").addEventListener("submit", handleLogin);
+document
+  .getElementById("register-form")
+  .addEventListener("submit", handleRegister);
